da2f6a25c788e69d19e476d32b97f8e38873658d
Nilesh Kahlkar <nilkcode111@gmail.com>
2022-09-27 10:05:42 +0530

    basic of js

---
+++ CouterApp/script.js
@@ -0,0 +1,29 @@
+
+
+// let myAge = 31;
+// console.log(myAge);
+
+let myAge = 31;
+let humanDogAgeRatio = 7;
+
+myDogAge = (myAge * humanDogAgeRatio);
+console.log(myDogAge);
+
+
+/*
+Create a variable, bonusPoint.Initilize it as 50. Increase as 100
+Decrease it down to 25, and finally increase it to 70
+
+*/
+
+let bonusPoint = 50;
+
+bonusPoint = bonusPoint + 50;
+console.log(bonusPoint);
+
+bonusPoint = bonusPoint - 75;
+console.log(bonusPoint);
+
+bonusPoint = bonusPoint + 145;
+console.log(bonusPoint);
+ 
